d2fe08af1e962f8d14913c3a9fce88fdad3fe79e
dinosause <140025369+dinosause@users.noreply.github.com>
2023-09-04 16:26:02 -0400

    Update crabs.js

--- crabs.js
+++ crabs.js
@@ -3,19 +3,19 @@
 function Fish1() {
     const node = document.getElementById("crabsjs-fish1");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone);
+    document.body.appendChild(clone);
 }
 
 function Fish2() {
     const node = document.getElementById("crabsjs-fish2");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone);
+    document.body.appendChild(clone);
 }
 
 function Fish3() {
     const node = document.getElementById("crabsjs-fish3");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone);
+    document.body.appendChild(clone);
 }
 
 var fishes = [Fish1, Fish2, Fish3]
@@ -30,37 +30,37 @@ function addFish() {
 function Car1() {
     const node = document.getElementById("crabsjs-car1");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone);
+    document.body.appendChild(clone);
 }
 
 function Car2() {
     const node = document.getElementById("crabsjs-car2");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone);
+    document.body.appendChild(clone);
 }
 
 function Car3() {
     const node = document.getElementById("crabsjs-car3");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone);
+    document.body.appendChild(clone);
 }
 
 function Car4() {
     const node = document.getElementById("crabsjs-car4");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone);
+    document.body.appendChild(clone);
 }
 
 function Car5() {
     const node = document.getElementById("crabsjs-car5");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone);
+    document.body.appendChild(clone);
 }
 
 function Car6() {
     const node = document.getElementById("crabsjs-car6");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone);
+    document.body.appendChild(clone);
 }
 
 var cars = [Car1, Car2, Car3, Car4, Car5, Car6]
@@ -75,5 +75,5 @@ function addCar() {
 function addGhost() {
     const node = document.getElementById("crabsjs-ghost");
     const clone = node.cloneNode(true);
-    document.getElementById("body").appendChild(clone); 
+    document.body.appendChild(clone); 
 }
